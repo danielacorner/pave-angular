8a42fa9b41cdb86f4b57d63a15a40125af331f77
Daniel Corner <dcorn068@gmail.com>
2018-08-20 21:41:33 -0400

    debug tooltip: markforcheck after each mouseover

--- src/app/d3-visuals/shared/node-visual.component.ts
+++ src/app/d3-visuals/shared/node-visual.component.ts
@@ -1,27 +1,27 @@
-import { Node } from './../../models/node';
-import { Component, Input } from '@angular/core';
+// import { Node } from './../../models/node';
+// import { Component, Input } from '@angular/core';
 
-@Component({
-  selector: 'app-node-visual',
-  template: `
-    <svg:g [attr.transform]="'translate(' + node.x + ',' + node.y + ')'">
-      <svg:circle
-          class="node"
-          [attr.fill]="node.color"
-          cx="0"
-          cy="0"
-          [attr.r]="node.r">
-      </svg:circle>
-      <svg:text
-          class="node-name"
-          [attr.font-size]="node.fontSize">
-        {{node.id}}
-      </svg:text>
-    </svg:g>
-  `,
-  styleUrls: ['./node-visual.component.css']
-})
-export class NodeVisualComponent {
-  @Input()
-  node: Node;
-}
+// @Component({
+//   selector: 'app-node-visual',
+//   template: `
+//     <svg:g [attr.transform]="'translate(' + node.x + ',' + node.y + ')'">
+//       <svg:circle
+//           class="node"
+//           [attr.fill]="node.color"
+//           cx="0"
+//           cy="0"
+//           [attr.r]="node.r">
+//       </svg:circle>
+//       <svg:text
+//           class="node-name"
+//           [attr.font-size]="node.fontSize">
+//         {{node.id}}
+//       </svg:text>
+//     </svg:g>
+//   `,
+//   styleUrls: ['./node-visual.component.css']
+// })
+// export class NodeVisualComponent {
+//   @Input()
+//   node: Node;
+// }
